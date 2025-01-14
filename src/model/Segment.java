b7235404eb211b9785e1cfed2066e5ed6d897a2d
REIGaruki <gyakujo@mail.ru>
2025-01-14 12:04:35 +0300

    Models

---
+++ src/model/Segment.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Segment {
+    private final LocalDateTime departureDate;
+
+    private final LocalDateTime arrivalDate;
+
+    public Segment(LocalDateTime departureDate, LocalDateTime arrivalDate) {
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+    }
+
+    public LocalDateTime getDepartureDate() {
+        return departureDate;
+    }
+
+    public LocalDateTime getArrivalDate() {
+        return arrivalDate;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter fmt =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        return '[' + departureDate.format(fmt) + '|' + arrivalDate.format(fmt)
+                + ']';
+    }
+}
