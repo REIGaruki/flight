dff3aaeb23c12102e14d07ead1b0325ea2d37001
REIGaruki <gyakujo@mail.ru>
2025-01-14 12:05:16 +0300

    Static filters

---
+++ src/service/FlightFilter.java
@@ -0,0 +1,50 @@
+package service;
+
+import model.Flight;
+import model.Segment;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightFilter {
+
+    static List<Flight> removeExpired(List<Flight> flights) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        return flights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getDepartureDate().isAfter(currentTime)))
+                .collect(Collectors.toList());
+    }
+
+    static List<Flight> removeArrivalBeforeDepartment(List<Flight> flights) {
+        return flights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getArrivalDate().isAfter(segment.getDepartureDate())))
+                .collect(Collectors.toList());
+    }
+
+    static List<Flight> removeMoreThanGroundtimeOnGround(List<Flight> flights) {
+        return flights.stream()
+                .filter(flight -> {
+                    List<Segment> segments = flight.getSegments();
+                    if (segments.size() <= 1) {
+                        return true;
+                    }
+                    Segment[] sorted = segments.stream()
+                            .sorted(Comparator.comparing(Segment::getDepartureDate))
+                            .toArray(Segment[]::new);
+                    long totalGroundTime = 0;
+                    for (int i = 1; i < sorted.length; i++) {
+                        LocalDateTime arrival = sorted[i-1].getArrivalDate();
+                        LocalDateTime departure = sorted[i].getDepartureDate();
+                        totalGroundTime += Duration.between(arrival, departure).toMinutes();
+                    }
+                    return totalGroundTime <= 120;
+                })
+                .collect(Collectors.toList());
+    }
+
+}
